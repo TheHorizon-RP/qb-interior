5b3df4c069df9030e47b78b2ad7b7a9348e7d1f7
MrAnderson <luketanderson1992@hotmail.co.uk>
2022-08-08 18:17:49 +0100

    new shells

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,10 +12,26 @@ client_scripts {
 }
 
 files {
-	'starter_shells_k4mb1.ytyp'
+	'starter_shells_k4mb1.ytyp',
+	'shellpropsv2.ytyp',
+	'shellpropsv3.ytyp',
+	'shellpropsv4.ytyp',	
+	'shellpropsv5.ytyp',
+	'shellpropsv7.ytyp',
+	'shellpropsv8.ytyp',
+	'shellpropsv16.ytyp'
+
+
 }
 
 -- Default (included)
 data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv5.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv7.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv8.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv16.ytyp'
 
 lua54 'yes'
